b3d5d4d6b9cda2c28c9316a384e58881e3d8c8f5
dredonse <60045075+dredonse@users.noreply.github.com>
2024-12-21 16:46:28 +0100

    Add files via upload

---
+++ 2 ImagenBD-PB/ImagenPB-BD/FrmInicial.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ImagenBD_PB
+{
+    partial class FrmInicial
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lbxIdImagenes = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbxImagen.Location = new System.Drawing.Point(260, 31);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(220, 219);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 0;
+            this.pbxImagen.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(296, 334);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(249, 37);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir de la aplicación";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lbxIdImagenes
+            // 
+            this.lbxIdImagenes.FormattingEnabled = true;
+            this.lbxIdImagenes.ItemHeight = 18;
+            this.lbxIdImagenes.Location = new System.Drawing.Point(30, 31);
+            this.lbxIdImagenes.Name = "lbxIdImagenes";
+            this.lbxIdImagenes.Size = new System.Drawing.Size(142, 220);
+            this.lbxIdImagenes.TabIndex = 4;
+            this.lbxIdImagenes.SelectedIndexChanged += new System.EventHandler(this.lbxIdImagenes_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 396);
+            this.Controls.Add(this.lbxIdImagenes);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pbxImagen);
+            this.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leer Imagen desde la  Base de Datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lbxIdImagenes;
+    }
+}
+
---
+++ 2 ImagenBD-PB/ImagenPB-BD/FrmInicial.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes; 
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagenBD_PB
+{
+    public partial class FrmInicial : Form
+    {
+        SqlConnection conexion;
+
+        public FrmInicial()
+        {
+            InitializeComponent();
+        }
+        
+        // Método asociado al evento de carga del formulario
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = "Server=ADTSERVER; DATABASE=adtjoaquinBDImagenes; Trusted_Connection=yes;";
+
+            // Creamos el objeto conexión, pasándole la cadena de conexión
+            conexion = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                // Intentamos la conexión
+                conexion.Open();
+
+                // Cargamos el ListBox con los id de las imágenes
+                cargarListBox();
+            }
+            catch
+            {
+                MessageBox.Show("Imposible realizar la conexión con la base de datos. Se cancela el programa", "Error de Aplicacion", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                btnSalir.PerformClick();
+            }
+        }
+
+        // Método asociado al botón btnSalir. Finaliza la aplicación
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (conexion != null) conexion.Close();     // Si la conexión está abierta, la cerramos
+            this.Close();                               // Cerramos el formulario
+        }
+
+        // Método asociado a seleccionar un Item en el ListBox
+        private void lbxIdImagenes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand();
+            SqlParameter parametroImagen;
+            SqlDataReader lector;
+            SqlBinary imagen = new SqlBinary();     // Requiere using System.Data.SqlTypes;    
+            MemoryStream ms = new MemoryStream();   // Requiere using System.IO;
+            Bitmap mapaBits;
+
+            // Establecemos el parámetro de la conexión
+            comando.Connection = conexion;
+
+            // Establecemos la orden, con parámetros
+            comando.CommandText = "SELECT Imagen FROM TImagenes WHERE idImagen = @pImagen";
+
+            // Definimos los parámetros con su tipo de dato
+            parametroImagen = new SqlParameter("pImagen", SqlDbType.Int);
+
+            // Damos valor al parámetro
+            parametroImagen.Value = lbxIdImagenes.SelectedItem;
+
+            // Añado el parámetro al comando
+            comando.Parameters.Add(parametroImagen);
+
+            try
+            {
+                // Ejecutamos el comando y recogemos un apuntador a los datos leidos
+                lector = comando.ExecuteReader();
+
+                // Leemos la fila, que será única, no puede haber dos imágenes con el mismo identificador
+                lector.Read();
+
+                // Cargamos los datos
+                imagen = lector.GetSqlBinary(0);
+
+                // La pasamos al Memory Stream
+                ms.Write(imagen.Value, 0, imagen.Length);
+
+                // Generamos el mapa de bits
+                mapaBits = new Bitmap(ms);
+
+                // Lo pasamos al PictureBox
+                pbxImagen.Image = mapaBits;
+
+                // Cerramos el lector
+                lector.Close();
+            }
+            catch
+            {
+                MessageBox.Show("No se pueden mostrar los datos", "Error de Aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }              
+        } 
+    
+            // Carga el ListBox con los identificadores de las imágenes
+        private void cargarListBox()
+        {
+            SqlDataReader lector;
+            SqlCommand comando = new SqlCommand();
+            
+            // Establecemos el parámetro de la conexión
+            comando.Connection = conexion;
+
+            // Establecemos la orden, con parámetros
+            comando.CommandText = "SELECT idImagen FROM TImagenes";
+          
+            try
+            {
+                // Ejecutamos el comando y recogemos un apuntador a los datos leidos
+                lector = comando.ExecuteReader();
+                
+                // Si el lector ha leido datos
+                if (lector.HasRows)
+                {
+                    // Leemos todas las filas de la tabla
+                    while (lector.Read())
+                    {
+                        // Cargamos los datos en el ListBox
+                        lbxIdImagenes.Items.Add(lector.GetInt32(0));
+                    }
+
+                    // Cerramos el lector
+                    lector.Close();
+
+                    // Seleccionamos el primer elemento del ListBox: Evento SelectedIndezChanged
+                    lbxIdImagenes.SetSelected(0, true);
+                }
+                else MessageBox.Show("No se pueden encontrar datos", "Aviso de Aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            catch
+            {
+                MessageBox.Show("No se pueden mostrar los datos", "Error de Aplicacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }                   
+        }
+    }
+}
